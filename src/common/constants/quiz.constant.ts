591ca3b8c6cda49ef21f23895d84747646ba930b
Omar Hashad <omarhashad88@gmail.com>
2026-07-26 19:16:35 +0300

    -Create constants dir

---
+++ src/common/constants/quiz.constant.ts
@@ -0,0 +1,10 @@
+
+export const QUIZ_PASSING_DEFAULT_THRESHOLD = 80;
+
+export const QUIZ_DEFAULT_INSTRUCTIONS = [
+  'Read each question carefully before selecting your answer.',
+  'Once you start the quiz, the timer cannot be paused or restarted.',
+  'Make sure you have a stable internet connection before beginning.',
+  'You must answer every question to submit the quiz.',
+  'Your result will be shown immediately after submission.',
+];
