e0c13bd4e9e08a396bf453d03c4f0d0bba3808bd
Omar Hashad <omarhashad88@gmail.com>
2026-07-26 19:52:10 +0300

    -Create Quiz controller -Create Quiz service

--- src/modules/quiz/quiz.service.ts
+++ src/modules/quiz/quiz.service.ts
@@ -4,16 +4,25 @@ import {
   DiplomaRepo,
   diplomaRepo,
 } from '../../common/repositories/diploma.repo.js';
+import { NotFoundException } from '../../common/utils/exception.util.js';
+import { QUIZ_PASSING_DEFAULT_THRESHOLD } from '../../common/constants/quiz.constant.js';
 export class QuizService {
   constructor(
     private readonly quizRepo: QuizRepo,
     private readonly diplomaRepo: DiplomaRepo,
   ) {}
 
-  async createQuiz(dto: createQuizDTO) {
+  async createQuiz(dto: createQuizDTO, photo: Express.Multer.File) {
     const diploma = await this.diplomaRepo.findOne({
       filter: { _id: dto.diplomaId },
-      options: { lean: true },
+    });
+
+    if (!diploma) throw new NotFoundException('diploma not found');
+    dto.passingThreshold =
+      dto.passingThreshold ?? QUIZ_PASSING_DEFAULT_THRESHOLD;
+
+    return await this.quizRepo.create({
+      data: { ...dto, photo: photo.filename },
     });
   }
 }
